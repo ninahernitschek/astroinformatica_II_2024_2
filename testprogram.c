133620baffb53c5bdc111eafe598838747303dcd
ninahernitschek <79425808+ninahernitschek@users.noreply.github.com>
2024-10-16 06:37:57 -0500

    Add files via upload

---
+++ testprogram.c
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+
+#include <cmath>
+
+
+
+int main() {
+  cout << "Hello World!";
+  
+  cout << sqrt(64)<<endl;
+  
+  cout << round(2.6)<<endl;
+  
+  cout << log(2)<<endl;
+
+  
+  
+  for (int i = 0; i <= 10; i = i + 2) {
+    cout << i << "\n";
+  }
+
+  return 0;
+} 
+ 
